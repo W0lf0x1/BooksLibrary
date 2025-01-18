5c50ec9a9354b36c790bf6d8172dd4962c35fa96
W0lf0x1 <131454305+W0lf0x1@users.noreply.github.com>
2025-01-18 10:07:16 +0500

    FirstCommit

--- index.php
+++ index.php
@@ -1,24 +1,57 @@
 <?php
-//Привязываю сюда классы
+
 include 'Book.php';
 include 'Reader.php';
 include 'Library.php';
 
-//Создаю библиотеку
+// Создаю библиотеку
 $library = new Library();
-//Добавляю книги для примера
+
+// Добавляю книги для примера
 $book1 = new Book("Лада против феррари", "Михаил Пушкин", 1999);
 $book2 = new Book("24 часа в чтз, пособие по выживанию", "Владимир Шишкин", 1960);
 $library->addBook($book1);
 $library->addBook($book2);
-//Добавляю читателя
+
+// Добавляю читателя
 $reader = new Reader("Данил Романов", "DaRo@gmail.com");
 $library->addReader($reader);
-//Читатель берет книгу
+
+// Вывожу информацию о читателе
+echo "Читатель: {$reader->getName()}, Email: {$reader->getEmail()}\n";
+
+// Вывожу список всех книг в библиотеке
+echo "\nСписок книг в библиотеке:\n";
+$library->listBooks();
+
+// Читатель берет книгу
+echo "\nЧитатель берет книгу '1984':\n";
 $reader->borrowBook($book1);
-//Список книг в библиотеке
+
+// Вывожу список книг после взятия
+echo "\nСписок книг после взятия:\n";
 $library->listBooks();
-//Читатель возвращает книгу
+
+// Вывожу информацию о взятых книгах читателя
+echo "\nКниги, взятые читателем:\n";
+foreach ($reader->getBorrowedBooks() as $borrowedBook) {
+    echo "- {$borrowedBook->getTitle()} ({$borrowedBook->getAuthor()}, {$borrowedBook->getYear()})\n";
+}
+
+// Читатель возвращает книгу
+echo "\nЧитатель возвращает книгу '1984':\n";
 $reader->returnBook($book1);
-//Список книг после возврата
+
+// Вывожу список книг после возврата
+echo "\nСписок книг после возврата:\n";
 $library->listBooks();
+
+// Вывожу информацию о взятых книгах читателя после возврата
+echo "\nКниги, взятые читателем после возврата:\n";
+if (empty($reader->getBorrowedBooks())) {
+    echo "Читатель не имеет взятых книг.\n";
+} else {
+    foreach ($reader->getBorrowedBooks() as $borrowedBook) {
+        echo "- {$borrowedBook->getTitle()} ({$borrowedBook->getAuthor()}, {$borrowedBook->getYear()})\n";
+    }
+}
