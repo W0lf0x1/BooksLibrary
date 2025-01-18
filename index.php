6242a61b90705d59c7cc339eda2a84c2219b446d
W0lf0x1 <131454305+W0lf0x1@users.noreply.github.com>
2025-01-18 09:34:24 +0500

    FirstCommit

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
+//Привязываю сюда классы
+include 'Book.php';
+include 'Reader.php';
+include 'Library.php';
 
+//Создаю библиотеку
+$library = new Library();
+//Добавляю книги для примера
+$book1 = new Book("1988", "Лада против феррари", 1999);
+$book2 = new Book("1955", "24 часа в чтз, пособие по выживанию", 1960);
+$library->addBook($book1);
+$library->addBook($book2);
+//Добавляю читателя
+$reader = new Reader("Данил Романов", "DaRo@gmail.com");
+$library->addReader($reader);
+//Читатель берет книгу
+$reader->borrowBook($book1);
+//Список книг в библиотеке
+$library->listBooks();
+//Читатель возвращает книгу
+$reader->returnBook($book1);
+//Список книг после возврата
+$library->listBooks();
